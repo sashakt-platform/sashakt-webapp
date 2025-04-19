29513224805b7e6e0c921625387cae4f2f477d00
akkshitgupta <96991785+akkshitgupta@users.noreply.github.com>
2025-04-20 00:57:38 +0530

    feat: add question card

---
+++ src/lib/components/Question.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card/index.js';
+
+	export type TQues = {
+		'S No': string;
+		State: string;
+		Questions: string;
+		'Option A': string;
+		'Option B': string;
+		'Option C': string;
+		'Option D': string;
+		'Correct Option': string;
+		'Training Tags': string;
+	};
+
+	let { ques, length }: { ques: TQues; length: Number } = $props();
+</script>
+
+<Card.Root class="mb-4">
+	<Card.Header>
+		<Card.Title class="mb-2 border-b p-2 text-xs">
+			<span>{ques['S No']} OF {length}</span>
+			<span class="text-muted-foreground float-end">{`1 Mark`}</span>
+		</Card.Title>
+		<Card.Description class="text-base/normal">{ques.Questions}</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<form
+			class="*:has-checked:bg-accent-foreground *:has-checked:text-accent flex w-full flex-col *:mb-1 *:rounded-lg *:border *:px-4 *:py-6 **:float-end"
+		>
+			<label for={ques['Option A']}>
+				A. {ques['Option A']}
+				<input type="radio" name={ques['S No']} id={ques['Option A']} value={ques['Option A']} />
+			</label>
+
+			<label for={ques['Option B']}>
+				B. {ques['Option B']}
+				<input type="radio" name={ques['S No']} id={ques['Option B']} value={ques['Option B']} />
+			</label>
+
+			<label for={ques['Option C']}>
+				C. {ques['Option C']}
+				<input type="radio" name={ques['S No']} id={ques['Option C']} value={ques['Option C']} />
+			</label>
+
+			<label for={ques['Option D']}>
+				D. {ques['Option D']}
+				<input type="radio" name={ques['S No']} id={ques['Option D']} value={ques['Option D']} />
+			</label>
+		</form>
+	</Card.Content>
+</Card.Root>
--- src/routes/test/[slug]/+page.svelte
+++ src/routes/test/[slug]/+page.svelte
@@ -1,15 +1,42 @@
 <script lang="ts">
 	import LandingPage from '$lib/components/LandingPage.svelte';
+	import QuestionCard from '$lib/components/Question.svelte';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Pagination from '$lib/components/ui/pagination/index.js';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
 	let isStarted = $state(false);
+	let currentQues = $state(0);
 </script>
 
 <section>
 	{#if !isStarted}
 		<LandingPage {data} bind:isStarted />
 	{:else}
-		<p>question palette</p>
+		<div class="mx-auto max-w-xl">
+			<QuestionCard ques={Questions[currentQues]} length={Questions.length} />
+			<Pagination.Root
+				count={Questions.length}
+				perPage={1}
+				class="fixed bottom-0 max-w-xl bg-white p-2"
+			>
+				{#snippet children({ currentPage })}
+					<Pagination.Content class="flex w-full items-center justify-between">
+						<Pagination.Item>
+							<Pagination.PrevButton onclick={() => (currentQues = currentPage)} />
+						</Pagination.Item>
+
+						<Pagination.Item>
+							{#if currentPage == Questions.length}
+								<Button>Submit</Button>
+							{:else}
+								<Pagination.NextButton onclick={() => (currentQues = currentPage)} />
+							{/if}
+						</Pagination.Item>
+					</Pagination.Content>
+				{/snippet}
+			</Pagination.Root>
+		</div>
 	{/if}
 </section>
