903ec89e335e488da077c7c7ed4ffd6514b78d2b
akkshitgupta <96991785+akkshitgupta@users.noreply.github.com>
2025-06-03 19:52:01 +0530

    feat: fetch candidate test questions

--- src/lib/server/test.ts
+++ src/lib/server/test.ts
@@ -13,3 +13,18 @@ export const getTestDetailsBySlug = async (slug: string) => {
 	const testData = await response.json();
 	return { testData };
 };
+
+export const getTestQuestions = async (candidate_test_id: number, candidate_uuid: string) => {
+	const apiEndpoint = `${BACKEND_URL}/candidate/test_questions/${candidate_test_id}/?candidate_uuid=${candidate_uuid}`;
+
+	const response = await fetch(apiEndpoint, {
+		method: 'GET',
+		headers: { accept: 'application/json' }
+	});
+
+	if (!response.ok) throw new Error('Test questions are not available');
+
+	const testQuestions = await response.json();
+
+	return testQuestions;
+};
