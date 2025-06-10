5fe3b3234240b67413b6894957354332e46613e1
Kurund Jalmi <kurund.jalmi@gmail.com>
2025-06-10 23:20:26 +0100

    update logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,11 @@
 </script>
 
 <nav class="mb-1 bg-gray-50 p-3">
-	<img src="/Frame.svg" alt="sashakt" class="w-40" />
+	<h2
+		class="w-ful text-primary scroll-m-20 pb-4 text-3xl font-extrabold tracking-tighter uppercase"
+	>
+		Sashakt
+	</h2>
 </nav>
 
 {@render children()}
