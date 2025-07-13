83c568128536a6f1427bba9cb2fc755cef113362
akkshitgupta <96991785+akkshitgupta@users.noreply.github.com>
2025-07-13 13:49:48 +0530

    fix: add vitest globals to avoid repetitive imports

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
 					name: 'client',
 					environment: 'jsdom',
 					clearMocks: true,
+					globals: true,
 					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
 					exclude: ['src/lib/server/**'],
 					setupFiles: ['./vitest-setup-client.ts']
