2dbbae0ebad4f1dab274cc989d00ca670f742ac3
Akshit Gupta <96991785+akkshitgupta@users.noreply.github.com>
2025-08-07 14:50:34 +0530

    fix: Show question instructions (#61)
    * fix: show question instructions
    
    * change to span

--- src/lib/components/QuestionCard.svelte
+++ src/lib/components/QuestionCard.svelte
@@ -77,10 +77,17 @@
 			{serialNumber} <span>OF {totalQuestions}</span>
 			<!-- <span class="text-muted-foreground float-end">{`1 Mark`}</span> -->
 		</Card.Title>
-		<Card.Description class="text-base/normal font-medium"
-			>{question.question_text}{#if question.is_mandatory}<span class="ml-1 text-red-500">*</span>
-			{/if}</Card.Description
-		>
+		<Card.Description class="text-base/normal font-medium">
+			{question.question_text}
+			{#if question.is_mandatory}
+				<span class="ml-1 text-red-500">*</span>
+			{/if}
+			{#if question.instructions}
+				<span class="text-muted-foreground mt-2 block text-sm">
+					{question.instructions}
+				</span>
+			{/if}
+		</Card.Description>
 	</Card.Header>
 
 	<Card.Content class="p-5 pt-1">
