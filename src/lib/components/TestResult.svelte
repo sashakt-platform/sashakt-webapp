a9be48a818a37301e1ef5f4d7941dfa40bbe87d4
Akshit Gupta <96991785+akkshitgupta@users.noreply.github.com>
2025-08-19 10:55:21 +0530

    Display candidate marks on Result Page(#64)
    * feat: show marks obtained by candidate
    
    * fix: show in fraction form

--- src/lib/components/TestResult.svelte
+++ src/lib/components/TestResult.svelte
@@ -45,10 +45,12 @@
 					<Table.Cell class="border-r">Not Attempted</Table.Cell>
 					<Table.Cell>{notAttempted}</Table.Cell>
 				</Table.Row>
-				<Table.Row class="font-semibold">
-					<Table.Cell class="border-r">Total marks obtained</Table.Cell>
-					<Table.Cell>{resultData.correct_answer} / {totalQuestions}</Table.Cell>
-				</Table.Row>
+				{#if resultData.marks_obtained !== null && resultData.marks_maximum !== null}
+					<Table.Row class="font-semibold">
+						<Table.Cell class="border-r">Total marks obtained</Table.Cell>
+						<Table.Cell>{resultData.marks_obtained} / {resultData.marks_maximum}</Table.Cell>
+					</Table.Row>
+				{/if}
 			</Table.Body>
 		</Table.Root>
 	{/if}
