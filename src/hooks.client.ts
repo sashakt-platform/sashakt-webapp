4dd11b0bbe3b4e534a8d6a6e2311c347a0052844
Kurund Jalmi <kurund.jalmi@gmail.com>
2026-01-24 18:08:40 +0000

    tweak sentry configuration

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,9 +1,13 @@
 import { dev } from '$app/environment';
+import { PUBLIC_APP_ENV } from '$env/static/public';
 import { handleErrorWithSentry, replayIntegration } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
 	dsn: 'https://37c8921ed45b4e66d2a436a3b2451f1d@o412613.ingest.us.sentry.io/4510144958431232',
+
+	// Disable Sentry in development mode
+	enabled: !dev,
 
 	tracesSampleRate: 1.0,
 
@@ -21,7 +25,7 @@ Sentry.init({
 	// If you don't want to use Session Replay, just remove the line below:
 	integrations: [replayIntegration()],
 
-	environment: dev ? 'development' : 'production'
+	environment: PUBLIC_APP_ENV || 'development'
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
