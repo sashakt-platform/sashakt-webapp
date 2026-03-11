0b7558d3e20c25ba36c48964648c22ae88f7684c
Ninad Khanolkar <ninad1701@gmail.com>
2026-03-11 14:30:10 +0530

    Updating Field Name

--- src/lib/components/Question.svelte
+++ src/lib/components/Question.svelte
@@ -151,7 +151,7 @@
 									{totalQuestions}
 									bind:selectedQuestions
 									showFeedback={testDetails?.show_feedback_immediately ?? false}
-									showMarkForReview={testDetails?.show_mark_for_review ?? true}
+									showMarkForReview={testDetails?.bookmark ?? true}
 								/>
 							</div>
 						{/each}
